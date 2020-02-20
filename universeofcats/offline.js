8f1d4a521c7c7a5b9d53e1feda440d4c7a7f00a7
Infinito por Descubrir Bahía Blanca <60552336+ixdbahia@users.noreply.github.com>
2020-02-20 16:53:40 -0300

    Agregado Universe of cats

---
+++ universeofcats/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1575988617,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/solidtile.png",
+		"images/jumpthrutile.png",
+		"images/jumpthru-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground5.png",
+		"images/tiledbackground6.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
